4668a15456682191efa52618ff2503188572b29b
Taylor Schinvelski <taylorbyks@hotmail.com>
2021-10-14 18:07:15 -0300

    feat: add proof of work

--- block.js
+++ block.js
@@ -2,15 +2,24 @@ const SHA256 = require('crypto-js/sha256');
 
 class Block {
   constructor(index, precedingHash = "", data) {
-    this.index = index;
-    this.timestamp = new Date;
-    this.data = data;
-    this.precedingHash = precedingHash;
-    this.hash = this.computeHash();
+    this.index = index
+    this.timestamp = new Date
+    this.data = data
+    this.precedingHash = precedingHash
+    this.nonce = 0
+    this.hash = this.computeHash()
+    this.proofOfWork(5)
   }
 
   computeHash() {
-    return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.precedingHash + this.timestamp + this.nonce + JSON.stringify(this.data)).toString()
+  }
+
+  proofOfWork(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++
+      this.hash = this.computeHash()
+    }
   }
 }
 
